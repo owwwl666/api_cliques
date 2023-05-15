5c051d8b33322b31a4365bf0e5a152808f5daf7c
owwwl666 <zaharsalam228@gmail.com>
2023-05-15 22:19:08 +1000

    Update cliques.py

--- cliques.py
+++ cliques.py
@@ -15,14 +15,11 @@ def shorten_link(url_shorten_link, token, href):
     return response.json()['id']
 
 
-def count_clicks(url_count_clincks, token, sum_clicks=0):
+def count_clicks(url_count_clincks, token):
     """Количество кликов по ссылке"""
     response = requests.get(url_count_clincks, headers=token)
     response.raise_for_status()
-    clicks = response.json()["link_clicks"]
-    for click in clicks:
-        sum_clicks += click["clicks"]
-    return sum_clicks
+    return response.json()["total_clicks"]
 
 
 def is_bitlink(url_is_bitlink, token):
@@ -37,7 +34,7 @@ def main():
     token = {'Authorization': f'Bearer {os.environ["TOKEN_BITLY"]}'}
     href_without_http = urlparse(href)._replace(scheme="").geturl()
     url_shorten_link = 'https://api-ssl.bitly.com/v4/shorten'
-    url_count_clincks = f'https://api-ssl.bitly.com/v4/bitlinks/{href_without_http}/clicks'
+    url_count_clincks = f'https://api-ssl.bitly.com/v4/bitlinks/{href_without_http}/clicks/summary'
     url_is_bitlink = f'https://api-ssl.bitly.com/v4/bitlinks/{href_without_http}'
     if is_bitlink(url_is_bitlink, token):
         clicks_count = count_clicks(url_count_clincks, token)
