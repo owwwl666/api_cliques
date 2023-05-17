8141e63a7e0278885a945128a78c6f50138946b6
owwwl666 <zaharsalam228@gmail.com>
2023-05-17 23:48:56 +1000

    Update cliques.py

--- cliques.py
+++ cliques.py
@@ -4,28 +4,28 @@ import requests
 from urllib.parse import urlparse
 
 
-def shorten_link(token, href):
+def shorten_link(request_header, href):
     """Сокращение ссылки"""
     url_shorten_link = 'https://api-ssl.bitly.com/v4/shorten'
     body = {
         "long_url": href,
     }
     response = requests.post(url_shorten_link, json=body,
-                             headers=token)
+                             headers=request_header)
     response.raise_for_status()
     return response.json()['id']
 
 
-def count_clicks(url_count_clincks, token):
+def count_clicks(url_count_clincks, request_header):
     """Количество кликов по ссылке"""
-    response = requests.get(url_count_clincks, headers=token)
+    response = requests.get(url_count_clincks, headers=request_header)
     response.raise_for_status()
     return response.json()["total_clicks"]
 
 
-def is_bitlink(url_is_bitlink, token):
+def is_bitlink(url_is_bitlink, request_header):
     """Проверка ссылки на сокращенность"""
-    response = requests.get(url_is_bitlink, headers=token)
+    response = requests.get(url_is_bitlink, headers=request_header)
     return response.ok
 
 
@@ -35,16 +35,16 @@ def main():
     href = input('Введите ссылку:')
     href_without_http = urlparse(href)._replace(scheme="").geturl()
 
-    token_bitly = {'Authorization': f'Bearer {os.environ["TOKEN_BITLY"]}'}
+    request_header = {'Authorization': f'Bearer {os.environ["TOKEN_BITLY"]}'}
 
     url_count_clincks = f'https://api-ssl.bitly.com/v4/bitlinks/{href_without_http}/clicks/summary'
     url_is_bitlink = f'https://api-ssl.bitly.com/v4/bitlinks/{href_without_http}'
 
-    if is_bitlink(url_is_bitlink, token_bitly):
-        clicks_count = count_clicks(url_count_clincks, token_bitly)
+    if is_bitlink(url_is_bitlink, request_header):
+        clicks_count = count_clicks(url_count_clincks, request_header)
         return f'Количетво кликов: {clicks_count}'
     try:
-        bitlink = shorten_link(token_bitly, href)
+        bitlink = shorten_link(request_header, href)
         return f'Битлинк: {bitlink}'
     except requests.exceptions.HTTPError:
         raise 'Некорректная ссылка'
